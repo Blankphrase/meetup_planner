9e50b01323850dd965323f13dfd01c91994588e6
Yuichi Hagio <yhagio87@gmail.com>
2015-11-26 21:03:02 -0500

    User can edit event

--- server/handlers/event-handlers.js
+++ server/handlers/event-handlers.js
@@ -1,6 +1,7 @@
 var conn   = require('../db_connection').defaultConnection;
 var Event  = conn.model('Event');
 var validations = require('../helpers/validations');
+var moment = require('moment');
 
 var events = exports;
 
@@ -15,8 +16,12 @@ events.getEvents = function(req, res){
   });
 };
 
+events.getCreateEventForm = function(req, res){
+  res.render('events/create_event', {user: req.user})
+};
+
 events.createEvent = function(req, res){
-  console.log(req.body);
+  // console.log(req.body);
   var name       = req.body.eventName;
   var type       = req.body.eventType;
   var host       = req.body.eventHost;
@@ -60,4 +65,90 @@ events.createEvent = function(req, res){
     return res.redirect('/');
   });
 
+};
+
+events.getOneEvent = function(req, res){
+  var isAuthor = false;
+
+  var eventId = req.params.eventid;
+
+  Event.findOne({_id: eventId}, function(err, event){
+    if (err) {
+      req.flash('error', 'Something went wrong. Try again.');
+      return res.redirect('/');
+    }
+
+    if(event.createdBy.equals(req.user._id)){
+      isAuthor = true;
+    }
+
+    return res.render('events/event_page', {user: req.user, event: event, isAuthor: isAuthor});
+  });
+};
+
+events.getEditEvent = function(req, res){
+
+  var eventId = req.params.eventid;
+
+  Event.findOne({_id: eventId}, function(err, event){
+    if (err) {
+      req.flash('error', 'Something went wrong. Try again.');
+      return res.redirect('/');
+    }
+
+    if(!event.createdBy.equals(req.user._id)){
+      req.flash('error', 'You are not author!');
+      return res.redirect('/');
+    }
+
+    var formattedStartDate = moment.utc(event.startDate).local().format();
+    var formattedEndDate = moment.utc(event.endDate).local().format();
+    
+    var _event = {
+      _id: event._id,
+      name: event.name,
+      type: event.type,
+      host: event.host,
+      createdBy: event.createdBy,
+      startDate: formattedStartDate,
+      endDate: formattedEndDate,
+      location: event.location,
+      message: event.message 
+    };
+
+    return res.render('events/edit_event', {user: req.user, event: _event});
+  });
+};
+
+events.postEditEvent = function(req, res){
+
+  var eventId = req.params.eventid;
+
+  var name       = req.body.eventName;
+  var type       = req.body.eventType;
+  var host       = req.body.eventHost;
+  var startDate  = req.body.startDate;
+  var endDate    = req.body.endDate;
+  var location   = req.body.location;
+  var message    = req.body.message || null;
+
+  var query = {_id: eventId};
+  var update = {
+    name: name,
+    type: type,
+    host: host,
+    startDate: startDate,
+    endDate: endDate,
+    location: location,
+    message: message
+  };
+
+  Event.findOneAndUpdate(query, update, function(err, event){
+    if (err) {
+      req.flash('error', 'Something went wrong. Try again.');
+      return res.redirect('/create_event');
+    }
+    req.flash('info', 'Successfully created!');
+    return res.redirect('/');
+  });
 };
--- server/routes.js
+++ server/routes.js
@@ -17,46 +17,8 @@ function checkIfLoggedIn(req, res, next){
 }
 
 module.exports = function(app){
-  // Dummy Events
-  // var events = [
-  //   {
-  //     name: 'Get fucked up!',
-  //     category: 'Social',
-  //     host: 'Alice Smith',
-  //     startDate: 'Dec 10, 2015 - 20:00',
-  //     endDate: 'Dec 11, 2015 - 05:00',
-  //     location: '456 DEF Street Phoenix, AZ',
-  //     message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  //   },
-  //   {
-  //     name: 'Get fucked up pt.2!',
-  //     category: 'Social',
-  //     host: 'Alice Smith',
-  //     startDate: 'Dec 12, 2015 - 20:00',
-  //     endDate: 'Dec 13, 2015 - 05:00',
-  //     location: '456 DEF Street Phoenix, AZ',
-  //     message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  //   }
-  // ];
 
- //  app.get('/', function(req, res){
-	// 	res.render('events/events', {user: req.user, events: events});
-	// });
   app.get('/', eventHandler.getEvents);
-
-	app.get('/create_event', ensureAuth, function(req, res){
-		res.render('events/create_event', {user: req.user})
-	});
-
-	app.post('/create_event', ensureAuth, eventHandler.createEvent);
-
-	// app.get('/edit_event', function(req, res){
-	// 	res.render('events/edit_event')
-	// });
-
-	// app.post('/edit_event', function(req, res){
-
-	// });
 
   /** Authentication routes
    * Handled by Auth Handlers (handlers/auth-handlers)
@@ -65,9 +27,17 @@ module.exports = function(app){
   app.post('/register', checkIfLoggedIn, authHandler.register);
   app.get('/login', checkIfLoggedIn, authHandler.getLoginPage);
   app.post('/login', checkIfLoggedIn, authHandler.login);
+  app.get('/logout', ensureAuth, authHandler.logout);
+
   // app.get('/forgot-password', checkIfLoggedIn, authHandler.getForgotPasswordPage);
   // app.post('/send-new-password', checkIfLoggedIn, email.sendNewPassword);
-  app.get('/logout', ensureAuth, authHandler.logout);
+
+	app.get('/create_event', ensureAuth, eventHandler.getCreateEventForm);
+	app.post('/create_event', ensureAuth, eventHandler.createEvent);
+  app.get('/events/:eventid', ensureAuth, eventHandler.getOneEvent);
+	app.get('/events/:eventid/edit_event', ensureAuth, eventHandler.getEditEvent);
+	app.post('/events/:eventid/edit_event', ensureAuth, eventHandler.postEditEvent);
+
 
   // app.get('/profile', ensureAuth, profile.getProfile);
   // app.post('/update-email', ensureAuth, profile.updateEmail);
