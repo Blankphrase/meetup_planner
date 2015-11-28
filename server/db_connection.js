4a4d3a19adb86e91b1255b1003361191c8d65a09
Yuichi Hagio <yhagio87@gmail.com>
2015-11-28 17:22:33 -0500

    on.error

--- server/db_connection.js
+++ server/db_connection.js
@@ -5,7 +5,13 @@ var eventModel = require('./models/event');
 function makeDefaultConnection() {
   var uri = process.env.MONGOLAB_URI || '127.0.0.1/meetupplanner';
   console.log('Connecting to DB : ', uri);
+  
   var conn = mongoose.createConnection(uri);
+
+  conn.on('error', function(err){
+    console.log('Connection Error ::: ', err);
+  });
+
   conn.model('User', userModel.userSchema);
   conn.model('Event', eventModel.eventSchema);
   return conn;
