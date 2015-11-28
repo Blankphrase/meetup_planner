13b91bd1b0f57e19f077546b818c7ae7c0d27968
Yuichi Hagio <yhagio87@gmail.com>
2015-11-27 19:15:50 -0500

    Fixed process.env.MONGO_URI

--- server/db_connection.js
+++ server/db_connection.js
@@ -5,7 +5,7 @@ var eventModel = require('./models/event');
 
 function makeDefaultConnection() {
    // var conn = mongoose.createConnection(config.dbHostName, config.port);
-   var conn = mongoose.createConnection(config.MONGO_URI);
+   var conn = mongoose.createConnection(process.env.MONGO_URI || config.dbHostName);
    conn.model('User', userModel.userSchema);
 	 conn.model('Event', eventModel.eventSchema);
    console.log('DB CONNECTED!');
