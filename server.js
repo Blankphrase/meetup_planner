1887c58a2c3256a12b1020e5cda7b4424885f289
Yuichi Hagio <yhagio87@gmail.com>
2015-11-28 18:24:28 -0500

    try catch

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ var mongoose  = require('mongoose');
 
 require('./express-config').expressSetup(app);
 
-var uri = process.env.MONGOLAB_URI || '127.0.0.1/meetupplanner';
-console.log('Connecting to DB : ', uri);
+try {
+  var uri = process.env.MONGOLAB_URI || '127.0.0.1/meetupplanner'; 
+  console.log('Connecting to DB : ', uri);
 
-mongoose.connect(uri, {}, function(err, db){
-  if(err){
-    console.log('Connection Error ::: ', err);
-  } else {
-    console.log('Successfully Connected!');
-  }
-});
+  mongoose.connect(uri, {}, function(err, db){ 
+  if(err) {  
+    console.log('Connection Error ::: ', err); 
+  } else { 
+    console.log('Successfully Connected!'); 
+  } 
+} catch (e) {
+   console.log("Caught exception trying to connect to database")
+   console.log(e)
+   console.log(e.message)
+   console.log(e.stack)
+} 
 
 require('./server/routes')(app);
 
