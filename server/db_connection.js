03295b0e6e92ec66bc94f6c9684ef7857b58094f
Yuichi Hagio <yhagio87@gmail.com>
2015-11-28 11:28:32 -0500

    try again

--- server/db_connection.js
+++ server/db_connection.js
@@ -2,12 +2,12 @@ var mongoose  = require('mongoose');
 var config    = require('../express-config');
 var userModel = require('./models/user');
 var eventModel = require('./models/event');
-var testURI = require('../config').testURI;
+// var testURI = require('../config').testURI;
 
 function makeDefaultConnection() {
   var uri = process.env.MONGOLAB_URI || config.dbHostName;
   console.log('Connecting to DB : ', uri);
-  var conn = mongoose.createConnection(testURI, function(err){
+  var conn = mongoose.createConnection(uri, function(err){
     if(err){
       console.log('CONNECTION ERROR::::', err);
     }
